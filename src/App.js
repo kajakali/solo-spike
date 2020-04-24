6a84a1ac1545b8505f4f1e7d1840f4b05bf8b7e7
Frieda Jacobson <kajakali@hotmail.com>
2020-04-24 15:53:54 -0500

    page loads with material ui installed

--- src/App.js
+++ src/App.js
@@ -2,25 +2,77 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+import { withStyles } from '@material-ui/core/styles/';
+
+import Button from '@material-ui/core/Button';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import Camera from '@material-ui/icons/Camera';
+
+
+
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+  rightIcon: {
+    marginLeft: theme.spacing(1),
+  },
+  iconSmall: {
+    fontSize: 20,
+  },
+});
+
+function App(props) {
+const { classes } = props;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <p>hello there. This is to make useful testing. I would like to play with
+          themes, and also with confirmation dialogs, and also perhaps with uploading images.
+          so I will make some buttons, style them with my theme, and then make confirmation dialog for them.
+          Then I will try uploading images using AWS S3? or field...something
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <Button variant="contained" color="primary" size="large" className={classes.button}>Click me, I'm primary!</Button>
+        <Button variant="outlined" color="secondary" size="small" className={classes.button}>Click me, I'm secondary</Button>
+        <Button className={classes.button}>Click me to get a confirmation dialogue</Button>
+        <Button variant="contained" color="primary" className={classes.button}></Button>
+        <input
+        accept="image/*"
+        className={classes.input}
+        id="contained-button-file"
+        multiple
+        type="file"
+      />
+      <label htmlFor="contained-button-file">
+        <Button variant="contained" component="span" className={classes.button}>
+          Upload 
+          <CloudUploadIcon className={`${props.classes.rightIcon} ${props.classes.iconSmall}`}/>
+        </Button>
+        <Camera />
+      </label>
+      </div>
+      
+    );
+  
 }
 
-export default App;
+
+export default withStyles(styles)(App);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from '@material-ui/core/styles/';
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import pink from '@material-ui/core/colors/pink';
+import red from '@material-ui/core/colors/red';
 import * as serviceWorker from './serviceWorker';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {main: purple[200]},
+    secondary: {main: pink[200]},
+    error: red,
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
